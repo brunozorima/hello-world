1df79d328fd784cd2855faf5a8187112c003319e
brunozorima <33598272+brunozorima@users.noreply.github.com>
2017-11-13 12:05:50 +0000

    Files for my portfolio

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BRUNO Ma
+ * Date: 27/06/2017
+ * Time: 20:06
+ */
+
+require 'index.html';
+
+?>
---
+++ index.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bruno Zorima - My personal website</title>
+    <meta name="author" content="Bruno Zorima" />
+    <meta name="description" content="Personal website of Bruno Zorima. Web Developer and Student in Birmingham, UK." />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class ="row">
+            <div class="col-md-2 col-sm-2 col-lg-2 col-offset-0">
+                <img id="profile-photo" class="img-thumbnail" src="images/profile.JPG" alt="Bruno Zorima" style="border-radius:50%" />
+
+                <p>
+                      <a href="https://github.com/brunozorima" title="GitHub">GitHub</a>&#8208;
+                       <a href="https://uk.linkedin.com/in/bruno-zorima-4a32b9146/" title="LinkedIn">LinkedIn</a>&#8208;
+                      <a href="https://twitter.com/brunozorima" title="Twitter">Twitter</a>
+                </p>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
